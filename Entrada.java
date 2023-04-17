aa51f3ea1d84c280ae6e6c9cc99059ce9974d5b7
oliver <galvezerwin28@gmail.com>
2023-04-17 13:17:52 +0200

    Incidencia 1

---
+++ Entrada.java
@@ -0,0 +1,3 @@
+public class Entrada{
+    public String path;
+}
--- main.java
+++ main.java
@@ -1,16 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-        Recursividad recursivo = new Recursividad();
-//        recursivo.metodoA();
-
-        System.out.println(recursivo.factorial(9));
-
 
         System.out.println("----------------------------------");
+		System.out.println("procesando............");
         Fibonacci f = new Fibonacci();
         System.out.println(f.posicionFibonacci(2,5));
         System.out.println(f.jp(2));
+
+	System.out.println("estoy dentrooooooooooooo");
     }
 }
