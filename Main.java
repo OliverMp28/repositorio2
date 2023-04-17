16b5d19904e3cc1dae9d9823ea64a832611ccbb6
oliver <galvezerwin28@gmail.com>
2023-04-17 13:35:24 +0200

    cambio de nombre2

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
-public class Main {
+ public class Main {
     public static void main(String[] args) {
 
 
         System.out.println("----------------------------------");
-		System.out.println("Computando....");
+		System.out.println("Computando.... ");
         Fibonacci f = new Fibonacci();
         System.out.println(f.posicionFibonacci(2,5));
         System.out.println(f.jp(2));
